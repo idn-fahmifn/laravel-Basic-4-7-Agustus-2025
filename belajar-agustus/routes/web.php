f1b5216da14260b23b1dda95d2f6e0bd8500ccf8
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-04 14:36:12 +0700

    1.4

--- belajar-agustus/routes/web.php
+++ belajar-agustus/routes/web.php
@@ -29,3 +29,22 @@ Route::get('mobil/{params}', function ($mobil) {
 Route::get('motor/{params?}', function ($motor = null) {
     return "Hallo, saya punya mobil mereknya : $motor";
 })->name('motor');
+
+
+// membuat group pada routing
+// digunakan ketika ada sebuah route yang memiliki kesamaan.
+Route::prefix('training')->group(function () {
+
+    // grup di dalam grup
+    Route::prefix('programming')->group(function () {
+        Route::get('laravel', function () {
+            return "Ini adalah training laravel";
+        });
+    });
+    Route::get('mtcna', function () {
+        return "Ini adalah training mtcna";
+    });
+    Route::get('ccna', function () {
+        return "Ini adalah training ccna";
+    });
+});
