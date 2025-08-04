5f08743d4f4944665d8b94863b1a11c75ce2293e
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-04 13:53:59 +0700

    1.2

--- belajar-agustus/routes/web.php
+++ belajar-agustus/routes/web.php
@@ -9,13 +9,23 @@ Route::get('/', function () {
 // mendefinisikan route
 // method get => menampilkan sebuah data atau halamamn.
 
-Route::get('profile-halaman', function(){
+Route::get('profile-halaman', function () {
     //menampilkan halaman index di folder profile
     return view('profile.index');
 });
 
-Route::get('profile', function(){
+Route::get('profile', function () {
     //menampilkan respon profile 
     return "ini adalah respon profile";
 });
 
+// Route parameter yang wajib dideklarasikan
+Route::get('mobil/{params}', function ($mobil) {
+    return "Hallo, saya punya mobil mereknya : $mobil";
+    // return 'saya punya mobil mereknya : ';
+});
+
+// Route parameter yang optional
+Route::get('motor/{params?}', function ($motor = null) {
+    return "Hallo, saya punya mobil mereknya : $motor";
+});
