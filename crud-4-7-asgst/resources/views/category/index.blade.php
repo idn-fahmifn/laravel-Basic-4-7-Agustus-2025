c5a92c44a57b09ca896c75a7e1ede5f4ae718404
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-06 13:46:23 +0700

    3.7

--- crud-4-7-asgst/resources/views/category/index.blade.php
+++ crud-4-7-asgst/resources/views/category/index.blade.php
@@ -3,24 +3,27 @@
 @section('content')
     <div class="card p-4">
         <div class="card-title">Kategori Produk</div>
-        <table class="table table-hover">
-            <thead>
-                <th>Nama Kategori</th>
-                <th>Jumlah Produk</th>
-                <th>Action</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                </tr>
-                <tr>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                </tr>
-            </tbody>
-        </table>
+
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <th>Nama Kategori</th>
+                    <th>Jumlah Produk</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>lorem</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
