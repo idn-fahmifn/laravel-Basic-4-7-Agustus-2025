6f49f0e1be2e80c2999b5fd3bdbe42717809b984
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-07 16:38:14 +0700

    4.14

--- pengaduan-4-7-agt/resources/views/pengaduan/create.blade.php
+++ pengaduan-4-7-agt/resources/views/pengaduan/create.blade.php
@@ -21,7 +21,7 @@
                         </div>
 
                         <div class="mt-4">
-                            <x-input-label for="jenis_laporan" :value="__('Judul Laporan')" />
+                            <x-input-label for="jenis_laporan" :value="__('Jenis Laporan')" />
                             <select name="jenis_laporan" class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
                                 <option value="kerusakan">Kerusakan</option>
                                 <option value="kehilangan">Kehilangan</option>
