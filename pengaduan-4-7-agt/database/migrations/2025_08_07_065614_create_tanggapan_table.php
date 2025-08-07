e59cecf80719d4433fe9138bc7685a0d4eccc3e6
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-07 14:46:35 +0700

    4.9

--- pengaduan-4-7-agt/database/migrations/2025_08_07_065614_create_tanggapan_table.php
+++ pengaduan-4-7-agt/database/migrations/2025_08_07_065614_create_tanggapan_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('tanggapan', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('laporan_id')->constrained('laporan')->cascadeOnDelete();
+            $table->text('tanggapan');
             $table->timestamps();
         });
     }
--- pengaduan-4-7-agt/routes/web.php
+++ pengaduan-4-7-agt/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// admin
+Route::prefix('admin')->middleware(['auth', 'verified', 'role'])->group(function(){
+    Route::get('dashboard', [DashboardController::class, 'admin'])->name('dashboard');
+});
+
+// user
+Route::prefix('user')->middleware(['auth', 'verified'])->group(function(){
+    Route::get('dashboard', [DashboardController::class, 'user'])->name('dashboard.user');
+});
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
