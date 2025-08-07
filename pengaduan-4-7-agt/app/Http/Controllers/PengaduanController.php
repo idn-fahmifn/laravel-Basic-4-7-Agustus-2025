6e3cc7b58d068c73861084d5f2c9b0b67f5a8e63
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-07 17:04:38 +0700

    4.17

--- pengaduan-4-7-agt/app/Http/Controllers/PengaduanController.php
+++ pengaduan-4-7-agt/app/Http/Controllers/PengaduanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PengaduanController extends Controller
@@ -10,4 +11,27 @@ class PengaduanController extends Controller
     {
         return view('pengaduan.create');
     }
+
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $request->validate([
+            'judul_laporan' => ['string', 'required', 'max:200'],
+            'dokumentasi' => ['file', 'required', 'max:2048', 'mimes:jpg,jpeg,png,svg,heic'],
+            'isi_laporan' => ['required'],
+            'jenis_laporan' => ['required'],
+        ]);
+
+        if ($request->hasFile('dokumentasi')) {
+            $gambar = $request->file('dokumentasi'); //file yang diupload
+            $path = 'public/images/pengaduan'; //path tempat menyimpan
+            $ext = $gambar->getClientOriginalExtension(); //mengambil nilai ext
+            $nama = 'dokumentasi_laporan_' . Carbon::now()
+            ->format('Ymdhis') . random_int(10, 100) .'.'. $ext; //nama setelah diupload
+            $gambar->storeAs($path, $nama);
+        }
+
+    }
+
 }
---
+++ pengaduan-4-7-agt/app/Models/Pengaduan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pengaduan extends Model
+{
+    protected $table = 'laporan';
+
+    protected $guarded;
+
+}
