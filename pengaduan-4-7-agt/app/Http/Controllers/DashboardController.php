3dba91ae2bde2ae93dc16ee7a13905afd9ab7bb4
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-07 15:02:57 +0700

    4.11

--- pengaduan-4-7-agt/app/Http/Controllers/DashboardController.php
+++ pengaduan-4-7-agt/app/Http/Controllers/DashboardController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function admin()
+    {
+        return view('dashboard.dashboard-admin');
+    }
+
+    public function user()
+    {
+        return view('dashboard.dashboard-user');
+    }
 }
